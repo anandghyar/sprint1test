3a8d91e7330162ee2768b5a78eed0627bf5054be
smita.nikhade <smita.nikhade@integrationqa.com>
2019-01-16 14:32:59 +1300

    Adding Data Parameterization changes

--- MRA_UI_RegressionTests/Tests/CorrectIncidentInformationIsDisplayedOnTheTurnoutScreenTest.cs
+++ MRA_UI_RegressionTests/Tests/CorrectIncidentInformationIsDisplayedOnTheTurnoutScreenTest.cs
@@ -7,6 +7,11 @@ namespace MobileResponseApplicationUI.Tests
     [TestFixture]
     public class CorrectIncidentInformationIsDisplayedOnTheTurnoutScreenTest : BaseScreen
     {
+        [OneTimeSetUp]
+        public void BeginExecution()
+        {
+            DataPool.PopulateInCollection("EventDetails.csv");
+        }
         [SetUp]
         public void TestInitialize()
         {
@@ -51,7 +56,11 @@ namespace MobileResponseApplicationUI.Tests
         {
             CloseApp();
         }
-
+        [OneTimeTearDown]
+        public void tearDown()
+        {
+            extent.Flush();
+        }
 
         public void WhenTheCadNumberIsTheReplayedOne()
         {
@@ -62,37 +71,61 @@ namespace MobileResponseApplicationUI.Tests
         public void ThenTheTurnOutScreenDisplaysCorrectTopBannerInfo()
         {
             var topBannerIncidentInfo = new TopBannerInfo(app);
-            Assert.That(topBannerIncidentInfo.topBannerEnvironment.Text, Is.EqualTo("Stg"));
-            Assert.That(topBannerIncidentInfo.topBannerCallSign.Text, Is.EqualTo("TEST001013"));
-            Assert.That(topBannerIncidentInfo.topBannerCPN.Text, Is.EqualTo("RIMUTAKA RAIL TUNNEL MAYMORN PORTAL"));
-            Assert.That(topBannerIncidentInfo.topBannerAddress.Text, Is.EqualTo("UNKN0003 PARKES LINE RD, MAYMORN, UPPER HUTT CITY"));
+            string environment = DataPool.ReadData(1, "Environment");
+            string callSign = DataPool.ReadData(1, "CallSign");
+            string cpn = DataPool.ReadData(1, "CPN");
+            string address1 = DataPool.ReadData(1, "Address1");
+            string address2 = DataPool.ReadData(1, "Address2");
+            string address3 = DataPool.ReadData(1, "Address3");
+            string address = address1 + ", " + address2 + "," + address3;
+            Assert.That(topBannerIncidentInfo.topBannerEnvironment.Text, Is.EqualTo(environment));
+            Assert.That(topBannerIncidentInfo.topBannerCallSign.Text, Is.EqualTo(callSign));
+            Assert.That(topBannerIncidentInfo.topBannerCPN.Text, Is.EqualTo(cpn));
+            Assert.That(topBannerIncidentInfo.topBannerAddress.Text, Is.EqualTo(address));
             Assert.IsTrue(topBannerIncidentInfo.topBannerBatteryPercentage.Displayed);
         }
 
         public void ThenTurnOutScreenDisplaysCorrectEventDetails()
         {
             var turnoutScreen = new TurnoutScreen(app);
-            Assert.That(turnoutScreen.turnoutTime.Text, Is.EqualTo("Turnout Time  13:44"));
-            Assert.That(turnoutScreen.turnoutDate.Text, Is.EqualTo("Tuesday, 10 April 2018"));
-            Assert.That(turnoutScreen.eventType.Text, Is.EqualTo("Structure Fire"));
-            Assert.That(turnoutScreen.eventdescription.Text, Is.EqualTo("SMOKE COMING FROM TUNNEL"));
+            string turnoutTime = DataPool.ReadData(1, "TurnoutTime");
+            string turnoutDate1 = DataPool.ReadData(1, "TurnoutDate1");
+            string turnoutDate2 = DataPool.ReadData(1, "TurnoutDate2");
+            string turnoutDate = turnoutDate1 + "," + turnoutDate2;
+            string eventType = DataPool.ReadData(1, "EventType");
+            string eventDescription = DataPool.ReadData(1, "EventDescription");
+            Assert.That(turnoutScreen.turnoutTime.Text, Is.EqualTo(turnoutTime));
+            Assert.That(turnoutScreen.turnoutDate.Text, Is.EqualTo(turnoutDate));
+            Assert.That(turnoutScreen.eventType.Text, Is.EqualTo(eventType));
+            Assert.That(turnoutScreen.eventdescription.Text, Is.EqualTo(eventDescription));
         }
 
         public void ThenTurnOutScreenDisplaysCorrectEventAddressDetails()
         {
             var turnoutScreen = new TurnoutScreen(app);
-            Assert.That(turnoutScreen.turnoutCPN.Text, Is.EqualTo("RIMUTAKA RAILWAY TUNNEL"));
-            Assert.That(turnoutScreen.turnoutFullAddress.Text, Is.EqualTo("UNKN0001 MAYMORN RD, MAYMORN, UPPER HUTT CITY"));
+            string cpn = DataPool.ReadData(1, "CPN");
+            string address1 = DataPool.ReadData(1, "Address1");
+            string address2 = DataPool.ReadData(1, "Address2");
+            string address3 = DataPool.ReadData(1, "Address3");
+            string address = address1 + ", " + address2 + "," + address3;
+            string dsa = DataPool.ReadData(1, "DSA");
+            string callSourceCode = DataPool.ReadData(1, "CallSourceCode");
+            string alarmLevel = DataPool.ReadData(1, "AlarmLevel");
+            Assert.That(turnoutScreen.turnoutCPN.Text, Is.EqualTo(cpn));
+            Assert.That(turnoutScreen.turnoutFullAddress.Text, Is.EqualTo(address));
             Assert.IsTrue(turnoutScreen.turnoutLocationChange.Displayed);
-            Assert.That(turnoutScreen.turnoutNearestDSAValue.Text, Is.EqualTo("1m"));
-            Assert.That(turnoutScreen.turnoutCallSourceCode.Text, Is.EqualTo("111"));
-            Assert.That(turnoutScreen.alarmLevelButton.Text, Is.EqualTo("1"));
+            Assert.That(turnoutScreen.turnoutNearestDSAValue.Text, Is.EqualTo(dsa));
+            Assert.That(turnoutScreen.turnoutCallSourceCode.Text, Is.EqualTo(callSourceCode));
+            Assert.That(turnoutScreen.alarmLevelButton.Text, Is.EqualTo(alarmLevel));
         }
 
         public void ThenTurnOutScreenDisplaysCorrectRespondingAppliance()
         {
             var turnoutScreen = new TurnoutScreen(app);
-            Assert.That(turnoutScreen.turnoutRespondingAppliance.Text, Is.EqualTo("TEST001012, TEST001013"));
+            string RespondingAppliance1 = DataPool.ReadData(1, "RespondingAppliance1");
+            string RespondingAppliance2 = DataPool.ReadData(1, "RespondingAppliance2");
+            string RespondingAppliance = RespondingAppliance1 + "," + RespondingAppliance2;
+            Assert.That(turnoutScreen.turnoutRespondingAppliance.Text, Is.EqualTo(RespondingAppliance));
         }
 
     }
